a2bc27ceb583bac2b538784e1bf7e00043211187
Juan Jouglard <jouglardjuan@gmail.com>
2020-03-19 14:06:05 -0300

    Change angular version

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static('frontEnd/personalPageFE/dist'));
+app.use(express.static('frontEnd/dist'));
 
 app.get('/api', (req, res) => res.json({ application: 'Reibo collection'}));
 
 app.get('/', (req,res) => {
-  res.sendFile(path.resolve('frontEnd/personalPageFE/dist/index.html'));
+  res.sendFile(path.resolve('frontEnd/dist/index.html'));
 });
 
-app.get('*', (req, res) => { 
-  res.sendFile(path.resolve('dist/index.html')); 
-});
 
 app.listen(8080,() => console.log('App Initialized'))
 
