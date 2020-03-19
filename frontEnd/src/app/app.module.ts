e33c769ce2b9e2bc9e00db5558a116091d54fa70
Juan Jouglard <jouglardjuan@gmail.com>
2020-03-19 14:08:32 -0300

    Credentials2

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 @NgModule({
-  declarations: [ 
+  declarations: [
     AppComponent
   ],
   imports: [
